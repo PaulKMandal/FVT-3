103ce0d9f0db80fc1a3590d272d9403cb0800caa
Paul Mandal <pkmandal221@gmail.com>
2025-09-26 21:26:30 -0500

    Added ParseArgs

--- server/main.py
+++ server/main.py
@@ -6,4 +6,16 @@ from concurrent.futures import ThreadPoolExecutor
 import grpc
 from common.proto import fvt3_pb2, fvt3_pb2_grpc
 
-AGENT_ID: str = ""
+
+def parse_args():
+    p = argparse.ArgumentParser(description="Minimal gRPC server")
+    p.add_argument("--id", required=True, help="Server id (e.g., server-01)")
+    p.add_argument("--host", default="0.0.0.0", help="Host to bind the gRPC server")
+    p.add_argument("--port", type=int, default=9001, help="Port to bind the gRPC server")
+    return p.parse_args()
+
+def main():
+    args = parse_args()
+
+if __name__ == "__main__":
+    main()
